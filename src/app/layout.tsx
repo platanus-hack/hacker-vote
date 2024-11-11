93035a21fe51779ab39ad75123355aeb5f36b083
chamox <szavila95@gmail.com>
2024-11-11 16:03:50 -0300

    feat(vercel): fix vercel URL

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,16 @@ import './globals.css'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import ReactQueryProvider from '@/providers/ReactQueryProvider'
 
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000'
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: 'Next.js and Supabase Starter Kit',
+  description: 'The fastest way to build apps with Next.js and Supabase',
+}
+
 export default function RootLayout({
   children,
 }: {
