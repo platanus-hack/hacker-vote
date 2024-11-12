3e96d900106595eb861b2091d7ac09a41d6eccc6
VicenteLavagnino <vicente.lavagnino@uc.cl>
2024-11-12 02:26:43 -0300

    feat(landing):  improve cards

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,30 +1,46 @@
+'use client'
+
+import React from 'react'
+
 const ProjectCard = ({ project }: { project: any }) => {
   return (
-    <div className="flex h-full flex-col overflow-hidden rounded bg-card shadow-lg dark:bg-gray-800">
-      <img
-        className="h-48 w-full object-cover"
-        src={project.logo_url || '/default-image.jpg'}
-        alt={project.project_name}
-      />
+    <div className="mx-auto h-96 w-96 rounded-lg bg-gray-800 shadow-lg transition-transform hover:shadow-black">
+      <div className="flex items-center p-3">
+        <div className="flex space-x-2">
+          <div className="h-3 w-3 rounded-full bg-red-500"></div>
+          <div className="h-3 w-3 rounded-full bg-yellow-400"></div>
+          <div className="h-3 w-3 rounded-full bg-green-500"></div>
+        </div>
+        <div className="ml-6 truncate text-sm text-white">
+          {project.project_name}.info
+        </div>
+      </div>
 
-      <div className="flex flex-grow flex-col justify-between px-6 py-4">
-        <h2 className="mb-4 text-center font-jetbrains-mono text-xl text-gray-900 dark:text-white">
-          {project.project_name}
-        </h2>
-
-        <div className="flex items-center justify-between gap-x-4">
-          <button
-            className="flex h-10 w-10 items-center justify-center rounded-full bg-primary font-jetbrains-mono text-white hover:bg-primary-foreground"
-            title="Me gusta"
-          >
+      <div className="flex h-auto flex-col items-start rounded-b-lg bg-gray-700 p-4">
+        <div className="flex w-full flex-col items-center space-y-4 rounded-md bg-gray-900 p-4 font-jetbrains-mono text-white">
+          <div className="w-full truncate text-left">
+            <span className="text-green-500">
+              cd {project.project_name}/logo.svg
+            </span>
+          </div>
+          <img
+            className="h-32 w-32 rounded-md object-cover shadow-md"
+            src={project.logo_url || '/default-image.jpg'}
+            alt={`${project.project_name} Logo`}
+          />
+          <div className="w-full truncate text-left">
+            <span className="text-green-500">
+              vim {project.project_name}/oneliner.txt
+            </span>
+          </div>
+          <div className="w-full truncate text-left">
+            {project.oneliner || 'Insert your oneliner here.'}
+          </div>
+        </div>
+        <div className="mt-4 flex w-full justify-center">
+          <button className="flex h-12 w-12 items-center justify-center rounded-full bg-gray-900 text-white shadow-lg transition-all hover:bg-green-500 focus:outline-none">
             👍
           </button>
-          <a
-            href={`/projects/${project.id}`}
-            className="rounded bg-secondary px-4 py-2 font-jetbrains-mono text-white hover:bg-secondary-foreground"
-          >
-            Details
-          </a>
         </div>
       </div>
     </div>
--- src/components/ProjectGrid.tsx
+++ src/components/ProjectGrid.tsx
@@ -3,9 +3,9 @@ import ProjectCard from './ProjectCard'
 const ProjectGrid = ({ projects }: { projects: any[] }) => {
   return (
     <div className="mx-auto w-full max-w-7xl px-6 py-10">
-      <div className="grid grid-cols-1 gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-20 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {projects.map((project) => (
-          <div key={project.id} className="flex h-full flex-col">
+          <div key={project.id} className="flex flex-col">
             <ProjectCard project={project} />
           </div>
         ))}
